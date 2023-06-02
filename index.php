bbcf333e4ca83b36a51638bc2789e89533cb3283
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-06-02 12:00:09 -0300

    commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Calculadora Simples</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action='<?php $_SERVER["PHP_SELF"] ?>' method="post">
+        <h1> Média Bimestral </h1>
+        <br>
+        <br>
+        <label for="nota1">Nota 1: </label>
+        <input type="number" max="10" name="nota1">
+        <br>
+        <label for="nota2">Nota 2: </label>
+        <input type="text" name="nota2">
+        <br>
+        <label for="nota3">Nota 3: </label>
+        <input type="text" name="nota3">
+        <br>
+        <label for="nota4">Nota 4: </label>
+        <input type="text" name="nota4">
+        <br>
+
+        <input type="submit"> <input type="reset" value="reset">
+    </form>
+
+    <?php
+
+        if(isset($_POST["nota1"]) && isset($_POST["nota2"]) && isset($_POST["nota3"]) && isset($_POST["nota4"])){
+        //Variáveis
+        $nota1 = 0;
+        $nota2 = 0;
+        $nota3 = 0;
+        $nota = 0;
+        $media = 0;
+        //Entrada
+        $nota1 = $_POST["nota1"];
+        $nota2 = $_POST["nota2"];
+        $nota3 = $_POST["nota3"];
+        $nota4 = $_POST["nota4"];
+        //Processamento 
+        $media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+        
+        if($media >= 7.5){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. "APROVADO", "</h2>";
+        }
+        }
+        if($media < 3){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. "REPROVADO", "</h2>";
+        }
+        if($media >= 3 && $media < 7.5){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. "RECUPERAÇÃO", "</h2>";
+        }
+        
+
+    ?>
+    
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Calculadora Simples</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action='<?php $_SERVER["PHP_SELF"] ?>' method="post">
+        <h1> Média Bimestral </h1>
+        <br>
+        <br>
+        <label for="nota1">Nota 1: </label>
+        <input type="number" max="10" name="nota1">
+        <br>
+        <label for="nota2">Nota 2: </label>
+        <input type="text" name="nota2">
+        <br>
+        <label for="nota3">Nota 3: </label>
+        <input type="text" name="nota3">
+        <br>
+        <label for="nota4">Nota 4: </label>
+        <input type="text" name="nota4">
+        <br>
+
+        <input type="submit"> <input type="reset" value="reset">
+    </form>
+
+    <?php
+        function getMedia(){
+            //Variavel
+            $soma = 0;
+
+            for($i = 1; $i < 5; $i++){
+                if(isset($_POST["nota" .$i])){
+                    $soma += $_POST["nota" .$i];
+                }
+            }
+            return $soma/4;
+        }
+        $media = getMedia();
+
+        if($media >= 7.5){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. " APROVADO", "</h2>";
+        }
+        if($media < 3){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. " REPROVADO", "</h2>";
+        }
+        if($media >= 3 && $media < 7.5){
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>", "Média Bimestral: "  .$media. " RECUPERAÇÃO", "</h2>";
+        }
+    ?>
+    
+</body>
+</html>
